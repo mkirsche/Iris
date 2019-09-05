d0b7ba67a0b711b4c5e25a5a8cb213db1e30c394
mkirsche <jnredotter@gmail.com>
2019-09-05 18:24:05 -0400

    Removed useless rm command in testing script

--- test/big_sim.sh
+++ test/big_sim.sh
@@ -5,7 +5,6 @@
 ../external_scripts/samtools sort bigaln.bam > bigaln_sorted.bam
 ../external_scripts/samtools index bigaln_sorted.bam
 ./sniffles -m bigaln_sorted.bam -n -1 -v bigsniffles.vcf
-rm base.fa.*
 rm base.fa-*
 rm bigaln.sam
 rm bigaln.bam
